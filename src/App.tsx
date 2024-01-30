9848c5fee7b833e8a9caa1732402fab299b706fb
EinarBalan <einar.balan@gmail.com>
2024-01-29 16:04:43 -0800

    moved website to root

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Markdown from 'react-markdown'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Markdown>
+          {"# hello world \n yo"}
+        </Markdown>
+      </div>
+    </>
+  )
+}
+
+export default App
